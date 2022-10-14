b73b8460243049962b922f5e02b763a2b98c0e65
anshg <anshkaushik232@gamil.com>
2022-10-14 21:40:40 +0530

    app for shopping

---
+++ lib/button_widget.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ButtonWidget extends StatelessWidget {
+  final String text;
+  final VoidCallback onClicked;
+
+  const ButtonWidget({
+    required this.text,
+    required this.onClicked,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => RaisedButton(
+    child: Text(
+      text,
+      style: TextStyle(fontSize: 24),
+    ),
+    shape: StadiumBorder(),
+    color: Theme.of(context).primaryColor,
+    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+    textColor: Colors.white,
+    onPressed: onClicked,
+  );
+}
+
+RaisedButton({required Text child, required StadiumBorder shape, required Color color, required EdgeInsets padding, required Color textColor, required VoidCallback onPressed}) {
+}
